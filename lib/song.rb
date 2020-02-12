0dfd98bb386e39fa189dacb91312b5dc5a243623
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-12 07:17:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,6 +37,8 @@ class Song
   def self.find_or_create_by_name(name)
     if self.find_by_name(name).nil?
       self.create_by_name(name)
+    else
+      self.find_by_name(name)
     end
   end
   
